29d727972ba8f29ce49a15f3b2a3c1fd9a657488
Connor Robb <cr821@byu.edu>
2024-05-06 09:41:12 -0600

    feat: added base route to serve static site

--- routes.js
+++ routes.js
@@ -1,7 +1,13 @@
 // routes.js
 const products = require('./products');
+const site = require('./site')
 
 module.exports = (fastify) => {
+
+    fastify.get('/', (req, reply) => {
+        reply.header('Content-Type', 'text/html').send(site.html)
+    })
+
     fastify.get('/api/products', (req, reply) => {
         reply.send(products.list);
     });
---
+++ site.js
@@ -0,0 +1,14 @@
+export const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <script src="products.js"></script>
+    <title>POS System</title>
+</head>
+<body>
+    <div id="poslist"></div>
+</body>
+</html>
+`
